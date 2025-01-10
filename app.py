d58deeaabf17322ce83dfb64f2dc8e11c174babd
Claudio Leite <claudiobl2010@gmail.com>
2025-01-10 15:44:05 -0300

    Adicionando template e static files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.template_folder = '%s/web/templates' % app.root_path
+app.static_folder = '%s/web/static' % app.root_path
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
